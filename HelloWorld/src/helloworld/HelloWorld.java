360bab943ff9156daab8fe9073f0722a8d58e291
Portatil <Portatil@192.168.1.33>
2016-06-28 17:19:24 +0200

    Prueba para subir

--- HelloWorld/src/helloworld/HelloWorld.java
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -15,7 +15,6 @@ public class HelloWorld {
   * @param args the command line arguments
   */
  public static void main(String[] args) {
-  // TODO code application logic here
+  System.out.println("Hello friend");
  }
- 
 }
