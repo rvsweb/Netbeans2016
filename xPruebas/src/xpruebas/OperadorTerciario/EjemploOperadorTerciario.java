925e8ef07faccc8edd14cf422c466caf126ff71f
Portatil <Portatil@192.168.1.33>
2016-07-04 18:03:59 +0200

    Ejemplos basicos

---
+++ xPruebas/src/xpruebas/OperadorTerciario/EjemploOperadorTerciario.java
@@ -0,0 +1,17 @@
+package xpruebas.OperadorTerciario;
+
+/**
+ * @see ->
+ * @since 2016
+ * @version 1
+ * @author Raul Vela Salas
+ */
+public class EjemploOperadorTerciario {
+
+ public static void main(String[] args) {
+
+  String pe = "pe1";
+  boolean si = (pe == "pe") ? true : false;
+  System.out.println(si);
+ }
+}
