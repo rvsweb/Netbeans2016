ad2fa133f01a753148b9adf8a389675ecd937eb7
Portatil <Portatil@192.168.1.33>
2016-07-04 21:13:31 +0200

    Merge origin/RamaLocal into RamaLocal T6 Lectura/Escritura Informacion

---
+++ LibroJavaRama/src/librojavarama/T6LecturaEscrituraInformacion/EjFlujoCaracteres/T6Pag151EjemploDeFlujoCaracteres.java
@@ -0,0 +1,19 @@
+package librojavarama.T6LecturaEscrituraInformacion.EjFlujoCaracteres;
+
+/**
+ * @see ->
+ * @since 2016
+ * @version 1
+ * @author Raul Vela Salas
+ */
+public class T6Pag151EjemploDeFlujoCaracteres {
+
+}
+
+class UsarFlujoCaracteres {
+
+ public static void main(String[] args) {
+
+  String s = "Flujo de Caracteres ";
+ }
+}
