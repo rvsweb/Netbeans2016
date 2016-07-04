7fe70abae50d1911b775fb5dcd326ae2053ecb4e
Portatil <Portatil@192.168.1.33>
2016-07-05 00:50:57 +0200

    Merge origin/RamaLocal into RamaLocal T6 Lectura/Escritura Informacion

--- Constribucion/src/constribucion/Constribucion.java
+++ Constribucion/src/constribucion/Constribucion.java
@@ -15,7 +15,7 @@ public class Constribucion {
   * @param args the command line arguments
   */
  public static void main(String[] args) {
-  // TODO code application logic here
+  System.out.println("Prueba subida");
  }
  
 }
