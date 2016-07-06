21fe02a60bbb332b8688e004aa9988f84edde911
Portatil <Portatil@192.168.1.33>
2016-07-06 10:13:27 +0200

    Merge master into RamaLocal Subir actualizacion codigo

--- LibroJavaRama/src/librojavarama/T6LecturaEscrituraInformacion/ClaseInputStreamReader_BufferedReader/T6Pag154UtilizacionFlujosPrintWriter.java
+++ LibroJavaRama/src/librojavarama/T6LecturaEscrituraInformacion/ClaseInputStreamReader_BufferedReader/T6Pag154UtilizacionFlujosPrintWriter.java
@@ -16,13 +16,22 @@ class UtilizarClasePrintWriter {
 
  public static void main(String[] args) {
 
-  PrintWriter pantalla = new PrintWriter(System.out);
-  char array[] = {'B', 'u', 'f', 'f', 'e', 'r', 'e', 'd', 'R', 'e', 'a', 'd', 'e', 'r'};
-  String str = new String("new InputStreamReader");
-  pantalla.write(array, 0, 14);
+  PrintWriter pantalla = new PrintWriter(System.out, false);
+  char array[] = {'P', 'r', 'i', 'n', 't', 'W', 'r', 'i', 't', 'e', 'r'};
+  String str = new String("\n Con flush desactivado necesita ejecutar al final el metodo flush");
+  pantalla.write(array, 0, 11);
   pantalla.print(" ");
   pantalla.write(str);
-  pantalla.println(" ! ! ! ");
-  pantalla.flush();
+  pantalla.println("!!!!!!!!!!!!!!!!!!!!"); // Se muestra porque el objeto no ha descargado toda la memoria que tiene almacenada
+  pantalla.flush(); // Este es mas eficiente.
+  pantalla.println("!!!!!!!!!!!!!!!!!!!!"); // No se muestra porque el objeto ha descargado toda la memoria despues del flush
+
+  PrintWriter pantalla1 = new PrintWriter(System.out, true);
+  char array1[] = {'P', 'r', 'i', 'n', 't', 'W', 'r', 'i', 't', 'e', 'r'};
+  String str1 = new String("\n Con flush activado NO necesita ejecutar al final el metodo flush");
+  pantalla1.write(array1, 0, 10);
+  pantalla1.print(" ");
+  pantalla1.write(str1);
+  pantalla1.println("\n ! ! ! ");
  }
 }
