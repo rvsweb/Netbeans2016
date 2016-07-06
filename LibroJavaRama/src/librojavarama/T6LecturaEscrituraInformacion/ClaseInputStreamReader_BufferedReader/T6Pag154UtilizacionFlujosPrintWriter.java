cc7336a79f540548d55931f71dcb51c3c1c25c71
Portatil <Portatil@192.168.1.33>
2016-07-06 09:09:13 +0200

    Uso PrintWriter Basico

---
+++ LibroJavaRama/src/librojavarama/T6LecturaEscrituraInformacion/ClaseInputStreamReader_BufferedReader/T6Pag154UtilizacionFlujosPrintWriter.java
@@ -0,0 +1,28 @@
+package librojavarama.T6LecturaEscrituraInformacion.ClaseInputStreamReader_BufferedReader;
+
+import java.io.PrintWriter;
+
+/**
+ * @see ->
+ * @since 2016
+ * @version 1
+ * @author Raul Vela Salas
+ */
+public class T6Pag154UtilizacionFlujosPrintWriter {
+
+}
+
+class UtilizarClasePrintWriter {
+
+ public static void main(String[] args) {
+
+  PrintWriter pantalla = new PrintWriter(System.out);
+  char array[] = {'B', 'u', 'f', 'f', 'e', 'r', 'e', 'd', 'R', 'e', 'a', 'd', 'e', 'r'};
+  String str = new String("new InputStreamReader");
+  pantalla.write(array, 0, 14);
+  pantalla.print(" ");
+  pantalla.write(str);
+  pantalla.println("");
+  pantalla.flush();
+ }
+}
