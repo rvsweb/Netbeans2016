d9cf3a41986ca63cf4bef4d7ab2290bdd237ff49
Portatil <Portatil@192.168.1.33>
2016-07-12 21:36:39 +0200

    Libro Rama

---
+++ xEjPagWeb/src/javacodegeeks/SaberTipoVariable/xEjSaberTipoVariable.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javacodegeeks.SaberTipoVariable;
+
+/**
+ * @see @since 2016
+ * @version 1
+ * @author Radwulf Candle
+ */
+public class xEjSaberTipoVariable {
+
+ public static void main(String[] args) {
+
+ }
+}
