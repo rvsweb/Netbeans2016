dbec554cb56c130dcf50c43d8b963476958d33f8
Portatil <Portatil@Portatil-HP>
2016-08-02 00:13:50 +0200

    Herencia 2 parte

---
+++ xPruebas/src/xpruebas/Herencia/xTestHerencia4.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package xpruebas.Herencia;
+
+/**
+ * @see 
+ * @since 2016
+ * @version 1
+ * @author Radwulf Candle
+ */
+public class xTestHerencia4 {
+
+}
