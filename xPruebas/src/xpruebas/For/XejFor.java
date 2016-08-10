3eea6b05bbe5faf9199ad833873469dc9c57bbdc
Portatil <Portatil@Portatil-HP>
2016-08-10 15:19:29 +0200

    Pruebas

---
+++ xPruebas/src/xpruebas/For/XejFor.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xpruebas.For;
+
+/**
+ * @see
+ * https://www.facebook.com/hakin9mag/photos/pb.225124664168558.-2207520000.1470741917./1420718927942453/?type=3&theater
+ * @since 2016
+ * @version 1
+ * @author Radwulf Candle
+ */
+public class XejFor {
+
+ public static void main(String[] args) {
+
+  for (int i = 0; i < 10; i++) {
+   System.out.print(i + " ");
+   System.out.print((i + 1) + " ");
+  }
+
+  System.out.println("\n ----------------------");
+  for (int i = 0; i < 10; i++) {
+   System.out.print((i + 1) + " ");
+  }
+  System.out.println("");
+ }
+ 
+ 
+ 
+}
