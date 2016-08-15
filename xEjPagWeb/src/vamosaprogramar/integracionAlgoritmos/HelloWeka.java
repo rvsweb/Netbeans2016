d78d8381f12f6e5cceae3d6754f3370ac6fcb46e
Portatil <Portatil@Portatil-HP>
2016-08-15 09:33:10 +0200

    Ejemplo 4º

---
+++ xEjPagWeb/src/vamosaprogramar/integracionAlgoritmos/HelloWeka.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vamosaprogramar.integracionAlgoritmos;
+
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.logging.Logger;
+import javax.sql.DataSource;
+import sun.security.jca.GetInstance.Instance;
+
+/**
+ * @see -
+ * http://vamosaprogramar.co/2016/08/14/integrando-weka-con-java-para-implementar-algoritmos-de-asociacion-paso-a-pasointeligencia-artificialnetbeans/
+ * @since 2016
+ * @version 1
+ * @author Radwulf Candle
+ */
+public class HelloWeka {
+
+ public static void main(String[] args) {
+
+  String dataset = "C:\\Users\\Portatil_user\\Documents\\NetBeans2016\\xEjPagWeb\\src\\vamosaprogramar\\integracionAlgoritmos\\basededatos.arff";
+
+//  DataSource source = new DataSource(dataset) {
+
+//   Instance data = source.getDataSet();
+
+//   Apriori model = new Apriori();
+
+//   model.buildAssociations (data);
+
+//   System.out.println (model);
+
+  }
+ }
