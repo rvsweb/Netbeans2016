78dd6f9f48cf94ca1c0eabd0fad475a986e7dca1
remoto <remoto@PC>
2016-08-27 11:03:37 +0200

    Vade 7º

---
+++ xPruebasDesktop/src/xpruebasdesktop/ScannerSwitch/xPruebasWhile.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xpruebasdesktop.ScannerSwitch;
+
+/**
+ * @see -
+ * @since
+ * @version
+ * @author Raul Vela Salas
+ */
+public class xPruebasWhile {
+
+ public static void main(String[] args) {
+
+  String a = "a1";
+  switch (a) {
+   case "a":
+    System.out.println("Soy " + a);
+    break;
+   default:
+    System.out.println("Otra cosa ");
+    break;
+  }
+ }
+}
